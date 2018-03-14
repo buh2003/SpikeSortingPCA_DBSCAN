10f674d3b354a4fec2a0431f7158828f5164dea8
buh2003 <36245945+buh2003@users.noreply.github.com>
2018-03-14 19:26:22 -0400

    Add files via upload

---
+++ Functions and Utilities/addPOItoPath_xlwrite.m
@@ -0,0 +1,8 @@
+function [  ] = addPOItoPath_xlwrite( )
+javaaddpath('poi-3.8-20120326.jar');
+javaaddpath('poi-ooxml-3.8-20120326.jar');
+javaaddpath('poi-ooxml-schemas-3.8-20120326.jar');
+javaaddpath('xmlbeans-2.3.0.jar');
+javaaddpath('dom4j-1.6.1.jar');
+end
+
