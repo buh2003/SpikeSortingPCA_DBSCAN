76c44b45a974d39e9773deb9222300e646474f5f
buh2003 <36245945+buh2003@users.noreply.github.com>
2018-03-15 19:04:43 -0400

    Update addPOItoPath_xlwrite.m

--- Functions and Utilities/addPOItoPath_xlwrite.m
+++ Functions and Utilities/addPOItoPath_xlwrite.m
@@ -1,7 +1,6 @@
 function [  ] = addPOItoPath_xlwrite( )
-xlwritepath=[pwd,'/','xlwrite'];
-xlwritepath=xlwritepath(1:end-7);
-javaaddpath([xlwritepath 'poi_library/poi-3.8-20120326.jar']);
+xlwritepath=fileparts(which('xlwrite'));
+javaaddpath([xlwritepath '/poi_library/poi-3.8-20120326.jar']);
 javaaddpath([xlwritepath 'poi_library/poi-ooxml-3.8-20120326.jar']);
 javaaddpath([xlwritepath 'poi_library/poi-ooxml-schemas-3.8-20120326.jar']);
 javaaddpath([xlwritepath 'poi_library/xmlbeans-2.3.0.jar']);
