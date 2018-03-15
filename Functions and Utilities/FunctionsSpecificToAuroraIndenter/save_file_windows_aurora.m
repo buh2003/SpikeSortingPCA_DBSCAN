a2c20f090e3ddfa35f8dbb4beecb644a2c3b5333
buh2003 <36245945+buh2003@users.noreply.github.com>
2018-03-15 12:56:00 -0400

    Update and rename save_file_windows.m to save_file_windows_aurora.m

--- Functions and Utilities/FunctionsSpecificToAuroraIndenter/save_file_windows_aurora.m
+++ Functions and Utilities/FunctionsSpecificToAuroraIndenter/save_file_windows_aurora.m
@@ -9,7 +9,7 @@
 % Save to excel function
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [ ] = save_file_windows(ALL_ISI_OF,StimPhase_ISI_OF,Dynamic_ISI_OF,Late_Static_ISI_OF, End_ISI_OF, Disp_points, dataName)
+function [ ] = save_file_windows_aurora(ALL_ISI_OF,StimPhase_ISI_OF,Dynamic_ISI_OF,Late_Static_ISI_OF, End_ISI_OF, Disp_points, latencies, dataName)
 headers = {'Stim #', 'Raw Disp', 'Raw Force', 'Disp', 'Force', '# Spikes', 'Mean ISI', 'SD ISI', 'CoV ISI', 'Min ISI', 'Medi ISI', 'Max ISI', ''};
 xlswrite([dataName ' analyzed.xlsx'],headers,'Dynamic'); 
 xlswrite([dataName ' analyzed.xlsx'],Dynamic_ISI_OF,'Dynamic', 'A2'); 
@@ -26,6 +26,8 @@ xlswrite([dataName ' analyzed.xlsx'],End_ISI_OF,'End', 'A2');
 headers_disp = {'Disp Start', 'Disp Peak', 'Disp Late', 'Disp End'};
 xlswrite([dataName ' analyzed.xlsx'],headers_disp,'Disp Points');
 xlswrite([dataName ' analyzed.xlsx'],Disp_points,'Disp Points', 'A2');
+xlswrite([dataName ' analyzed.xlsx'],{'Force','Latency to Spike'},'Latencies');
+xlswrite([dataName ' analyzed.xlsx'],[ALL_ISI_OF(:,5),latencies],'Latencies', 'A2');
 disp('Saving.......Done!')
 end
 
