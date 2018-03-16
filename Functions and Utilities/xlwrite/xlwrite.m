ddc529c39abd039eb490050bd4ff84f35723375d
buh2003 <36245945+buh2003@users.noreply.github.com>
2018-03-16 14:50:02 -0400

    Update xlwrite.m

--- Functions and Utilities/xlwrite/xlwrite.m
+++ Functions and Utilities/xlwrite/xlwrite.m
@@ -85,13 +85,6 @@ function [status, message]=xlwrite(filename,A,sheet, range)
 %   Copyright 2012-2013, Alec de Zegher
 %==============================================================================
 
-% Load POI lib to java path
-%     javaaddpath /Users/benjaminhoffman/Documents/MATLAB/20130227_xlwrite/poi_library/poi-3.8-20120326.jar;
-%     javaaddpath /Users/benjaminhoffman/Documents/MATLAB/20130227_xlwrite/poi_library/poi-ooxml-3.8-20120326.jar;
-%     javaaddpath /Users/benjaminhoffman/Documents/MATLAB/20130227_xlwrite/poi_library/poi-ooxml-schemas-3.8-20120326.jar;
-%     javaaddpath /Users/benjaminhoffman/Documents/MATLAB/20130227_xlwrite/poi_library/xmlbeans-2.3.0.jar;
-%     javaaddpath /Users/benjaminhoffman/Documents/MATLAB/20130227_xlwrite/poi_library/dom4j-1.6.1.jar;
-
 addPOItoPath_xlwrite( )
 
 % Check if POI lib is loaded
